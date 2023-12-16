a3434cf2eeb8979b8e4584ba4af4d8996da90618
EmersonCcp <emerson.paixao52@gmail.com>
2023-12-15 21:45:07 -0300

    a

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('detail/<int:article_id>', views.detail, name='detail'),
     path('edit/<int:article_id>', views.edit, name='edit'), 
     path('delete/<int:article_id>', views.delete, name='delete'), 
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
--- dproject/settings.py
+++ dproject/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig', 
-    "debug_toolbar"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
